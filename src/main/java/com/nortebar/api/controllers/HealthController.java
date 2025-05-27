8ec8ce6bdd60c3956246f8cd3c5345579b450b01
Franco Talles <fperez@digiboard.com.br>
2025-05-27 16:37:27 -0400

    adicionando a primeira rota da aplicaçcao

---
+++ src/main/java/com/nortebar/api/controllers/HealthController.java
@@ -0,0 +1,15 @@
+package com.nortebar.api.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+
+    @GetMapping
+    public String estaSaudavel() {
+        return "Api está online e funcionando perfeitamente";
+    }
+}
