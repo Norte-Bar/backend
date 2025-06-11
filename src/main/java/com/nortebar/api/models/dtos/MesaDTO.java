87b06ac9cfe3518792efa0f97923b3450730f36f
Franco Talles <fperez@digiboard.com.br>
2025-06-11 08:06:41 -0400

    corrigindo dto da mesa

--- src/main/java/com/nortebar/api/models/dtos/MesaDTO.java
+++ src/main/java/com/nortebar/api/models/dtos/MesaDTO.java
@@ -1,12 +1,15 @@
 package com.nortebar.api.models.dtos;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 
 public record MesaDTO(
 
-    @NotBlank
+    @NotNull(message = "O número da mesa é obrigatório")
+    @Min(value = 1, message = "O número da mesa deve ser maior que zero")
     Integer numero,
 
-    @NotBlank
+    @NotNull(message = "A capacidade da mesa é obrigatório")
+    @Min(value = 1, message = "A capacidade da mesa deve ser maior que zero")
     Integer capacidade
 ) {}
